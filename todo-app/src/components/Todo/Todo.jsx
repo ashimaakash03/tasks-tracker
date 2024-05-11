f19891737cad70a1d14edda9bcfdb442078e2785
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-11 17:51:24 +0530

    corrected display of task items list

--- todo-app/src/components/Todo/Todo.jsx
+++ todo-app/src/components/Todo/Todo.jsx
@@ -1,15 +1,53 @@
+import { useEffect, useRef, useState } from "react";
 import "./Todo.css";
+import TodoItems from "../TodoItems/TodoItems";
 
+let count = 0;
 const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+
+  const addTask = () => {
+    setTodos([
+      ...todos,
+      { number: count++, text: inputRef.current.value, display: " " },
+    ]);
+    inputRef.current.value = "";
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
   return (
     <div className="todo">
       <div className="todo-header">TO DO LIST</div>
       <div className="todo-add">
-        <input type="text" placeholder="Add Task..." className="todo-input" />
-        <div className="todo-add-btn">ADD</div>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Add Task..."
+          className="todo-input"
+        />
+        <div
+          onClick={() => {
+            addTask();
+          }}
+          className="todo-add-btn"
+        >
+          ADD
+        </div>
       </div>
       <div className="todo-list">
-
+        {todos.map((item, index) => {
+          return (
+            <TodoItems
+              key={index}
+              number={item.number}
+              display={item.display}
+              text={item.text}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- todo-app/src/components/TodoItems/TodoItems.jsx
+++ todo-app/src/components/TodoItems/TodoItems.jsx
@@ -1,7 +1,25 @@
 import "./TodoItems.css";
+import tick from "../../assets/images/tick.png";
+import cross from "../../assets/images/cross.png";
+import not_tick from "../../assets/images/not_tick.png";
 
-const TodoItems = () => {
-  return <div></div>;
+// eslint-disable-next-line react/prop-types
+const TodoItems = ({ number, text }) => {
+  return (
+    <div className="todo-items">
+      <div className="todo-items-container">
+        <img src={not_tick} alt="" />
+        
+        <div className="todo-items-text">
+          Task#{number + 1}. {text}
+        </div>{" "}
+        <div className="task-action">
+          <img src={cross} alt="" />
+          <img src={tick} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TodoItems;
