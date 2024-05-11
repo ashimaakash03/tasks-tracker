fed8b9260527190a599aa387f9f80fd1b9af123c
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-11 18:09:26 +0530

    solved data persistency issue

--- todo-app/src/components/Todo/Todo.jsx
+++ todo-app/src/components/Todo/Todo.jsx
@@ -16,7 +16,13 @@ const Todo = () => {
   };
 
   useEffect(() => {
-    console.log(todos);
+    setTodos(JSON.parse(localStorage.getItem("todos")));
+  }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(todos);
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, 100);
   }, [todos]);
   return (
     <div className="todo">
